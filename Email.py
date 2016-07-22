55edff066513b6325c999e8ace192649d5c902aa
Quoc Le <Quoc125@gmail.com>
2016-07-22 08:59:15 -0400

    TripWire

---
+++ Email.py
@@ -0,0 +1,64 @@
+'''
+Created on Jul 4, 2016
+
+@author: Brandon
+'''
+import os
+import smtplib
+# import Email
+# import email.mime.multipart
+# import email.mime.text
+# import email.mime.base
+# import email.mime.image
+# from email import encoders
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+#from smtplib import toaddrs
+# from macpath import basename
+
+#if __name__ == '__main__':
+#    pass
+
+class Email(object):
+
+    def __init__(self, toaddr, filename):
+        self.toaddr = toaddr                        #sets the address the email will be sent to
+        self.filename = filename                    #sets the name of the picture file to send
+
+    def SendEmail(self):
+        fromaddr = "tripwiretestemail@gmail.com"    #email address that will be sending the email
+
+        msg = MIMEMultipart()                       #Allows the message to have multiple parts
+
+        msg['From'] = fromaddr
+        msg['To'] = self.toaddr
+        msg['Subject'] = "Test Email"
+
+        body = "Test"
+
+        msg.attach(MIMEText(body, 'plain'))
+
+
+        attachment = open( self.filename, "rb").read()     #get the picture file to attach
+        image = MIMEImage(attachment, name=os.path.basename(self.filename))   
+        # part = ('application', 'octet-stream')
+        # part.set_payload((attachment).read())
+        # encoders.encode_base64(part)
+        # part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+        msg.attach(image)
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, "tripwire")
+        text = msg.as_string()
+        server.sendmail(fromaddr, self.toaddr, text)
+        server.quit()
+
+
+    # try:
+
+        # print ("Successfully sent email")
+    # except smtplib.SMTPException:
+        # print ("Error: unable to send email")
---
+++ cam.py
@@ -0,0 +1,14 @@
+import cv2
+frame = 10
+class cam:
+    def __init__(self, filename):
+        self.filename = filename                    #sets the name of the picture file to send
+
+    def takePic(self):
+        camera = cv2.VideoCapture(0)
+        #workup the camera
+        temp = camera.read()
+        im = camera.read()
+        cv2.imwrite(self.filename, im[1])
+        del(camera)
+
