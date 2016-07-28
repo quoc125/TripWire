af8672d1517a9dc3c4be1763c80b49ce5d60753f
Quoc Le <Quoc125@gmail.com>
2016-07-28 16:50:01 -0400

    Changes to be committed: 	modified:   update.py

--- update.py
+++ update.py
@@ -1,3 +1,11 @@
+'''
+Created on Jul 28, 2016
+TripWire
+@author: Brandon Royal
+         Quoc Le
+         Sayeed Tahseen
+         Gilbert Vieux
+'''
 from BeautifulSoup import BeautifulSoup, Tag
 import datetime
 import os
@@ -6,8 +14,37 @@ class update(object):
         self.filename = filename                    #sets the name of the picture file to send
     def updateHTML(self):
         #open the html that is going to be updated
-        html = open('index.html',"r+")
-        soup = BeautifulSoup(html)
+        fileOpened = False
+        #if the index existed, update the file
+        try:
+            html = open('index.html',"r+")
+            soup = BeautifulSoup(html)
+            fileOpened = True
+        #if opening the file failed, created a new index.html file
+        except IOError:
+            html = """
+            <!DOCTYPE html>
+            <html>
+             <head>
+              <title>
+               Storage
+              </title>
+             </head>
+             <body>
+              <h1 style="text-align:center:">
+               Trip Wire
+              </h1>
+              <h2 style="text.align:center;">
+               Team 5
+              </h2>
+              <h3 style="padding-left:  3px;">
+               Client Photos
+              </h3>
+             </body>
+            </html>
+            """
+            soup = BeautifulSoup(html)
+
 
         #define object that will be added to the html
         date =  BeautifulSoup("<p> "+str(datetime.datetime.now()) +"</p>")
@@ -17,7 +54,9 @@ class update(object):
         soup.body.insert(len(soup.body.contents),date)
         soup.body.insert(len(soup.body.contents),temp)
 
-        #update the html
-        html.close()
+        #close the file, it it existed
+        if fileOpened == True:
+            html.close()
+        #write the index.html
         with open("index.html", "wb") as file:
             file.write(soup.prettify())
